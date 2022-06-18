4f41d21e829020173592595852bbef9df2015138
Angelita Santos <angelitasantos@gmail.com>
2022-06-18 11:52:37 -0300

    create routers urls

--- backend/imdb/urls.py
+++ backend/imdb/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from movies.api import viewsets as moviesviewsets
+
+route = routers.DefaultRouter()
+route.register(r'movies', moviesviewsets.MoviesViewSet, basename="Movies")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(route.urls)),
 ]
