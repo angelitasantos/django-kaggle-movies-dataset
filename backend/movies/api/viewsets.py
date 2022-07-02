58ee253776fc3404d185025481b687106196ab6d
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2022-07-02 14:50:03 -0300

    Delete backend directory

--- backend/movies/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from movies.api import serializers
-from movies import models
-
-class MoviesViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.MoviesSerializer
-    queryset = models.Movies.objects.all()
